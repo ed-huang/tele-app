2ac34b1ffceefd99e82984673ece0638099989f7
Ed <ed@edhuang.com>
2014-08-18 14:16:55 +0800

    started login route and controoler

--- app/router.js
+++ app/router.js
@@ -15,6 +15,8 @@ Router.map(function() {
     this.route('followers');  
   });
   this.route('dashboard');
+  this.route('create-account/sign-in');
+  this.route('create-account/login');
 });
 
 export default Router;
