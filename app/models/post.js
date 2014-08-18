58eecdffd997b95f35f233079468956829659e39
Ed <ed@edhuang.com>
2014-08-18 11:49:23 +0800

    updated styles for user posts

--- app/models/post.js
+++ app/models/post.js
@@ -12,25 +12,25 @@ Post.reopenClass({
     id: '1',
       author: 'cristianstrat', 
       text: 'Great teams constantly learn and re-learn how to move from the ego of *I* to the ego of *WE*.',
-      timestamp: '1407914948165'
+      timestamp: '2 minutes ago'
   },
   {
     id: '2',
       author: 'clarkewolfe', 
       text: 'Listen, I don\'t want to brag about my awesome #gaming skills but someone made it into an @IGN article today...',
-      timestamp: '1407914948165'
+      timestamp: '5 minutes ago'
   },
   { 
     id: '3',
       author: 'fastcompany', 
       text: 'THIS APP IS LIKE A REMOTE CONTROL FOR YOUR CREDIT CARDS',
-      timestamp: '1407914948165'
+      timestamp: '7 minutes ago'
   },
   { 
     id: '4',
       author: 'fastcompany', 
       text: 'Leica is celebrating its 100th birthday by launching an entirely new camera system. Born out of a design partnership with Audi, the unibody Leica T is an APS-C-sensored minimalistic masterpiece.',
-      timestamp: '1407914948165'
+      timestamp: '8 minutes ago'
   }
   ]
 });
--- app/routes/user/following.js
+++ app/routes/user/following.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {
-		console.log('user/following');
 		return this.store.find('user');
 	}
 });
