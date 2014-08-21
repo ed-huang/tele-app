6253e73722f7ca6b856af14ff075a752b5d1cb0c
Ed <ed@edhuang.com>
2014-08-21 12:27:00 +0800

    made updates from andres comments

--- app/routes/create-account/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-    model: function() {
-        console.log("is this working!!!!!");
-        return this.store.find('user');
-    }
-    
-});
--- app/routes/user.js
+++ app/routes/user.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-    model: function(params) {
-        console.log('params: ', params);
-        return this.store.find('user', params.user_id);
-    },
-
     actions: {
         error: function(reason) {
             console.log('reason: ', reason);
