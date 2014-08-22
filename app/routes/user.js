07cd08a5841d4a84667a4c647c0e6174f3ef5fce
Ed <ed@edhuang.com>
2014-08-22 17:57:24 +0800

    updated posts page

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,10 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function(params) {
-        // debugger;
         var self = this;
-        // var u = this.store.find('user', params.user_id);
-        // console.log('u: ', u);
         return this.store.find('user', params.user_id).then( null, function() {
             self.transitionTo('create-account');
         });
--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    
+
     model: function() {
-        return this.store.find('post');
+        // console.log('userId: ', this.get('user'), user);
+        var post = this.store.find('post');
+        var curUserId = this.modelFor('user').get('id');
+        console.log('model: ', curUserId);
+        //How do i filter the posts withe the curUser ID???
+        console.log('post author: ', post.get('author   '));
+
+        return post;
     }
 });
