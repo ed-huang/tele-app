a39544dbcd33bce494ea8d167b8efac75c166522
Ed <ed@edhuang.com>
2014-09-06 10:15:01 -0700

    changes for using passport

--- app/controllers/create-account/login.js
+++ app/controllers/create-account/login.js
@@ -16,14 +16,13 @@ export default Ember.Controller.extend({
 //Check to see if form name and password within the DB
             if (username && password) {
 //FIND generates a GET
+                console.log('logging in: ');
                 store.find('user', { username: username, password: password, operation: 'login' } ).then(function(users) {
+                    
+                    console.log('user: ', users);
+                    self.set('session.user', users.get('firstObject'));
+                    self.transitionToRoute('dashboard');
 
-                        // if (user.get('password') === password) {
-                            //lookup firstObject
-                            console.log('got it: ', users.get('firstObject') );
-                            self.set('session.user', users.get('firstObject'));
-                            self.transitionToRoute('dashboard');
-                        // }
                 });
             } else {
                 console.log('no username in database');
