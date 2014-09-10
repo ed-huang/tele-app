145b3d09f6e7e282618d787483675bab2534ba09
Ed <ed@edhuang.com>
2014-09-10 15:54:58 -0700

    added delete post test

--- app/components/user-post.js
+++ app/components/user-post.js
@@ -3,24 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
         delete: function() {
-
             var post = this.get('model');
+            var that = this.$().clone();
+            this.$().prev().before(that);
+            that.hide('slow', function() {
+                that.remove();
+            });
+            
             post.deleteRecord();
             post.save();
             
         }
     },
-
-    didInsertPost: function() {
-        console.log('inserted a new post');
+    
+    insertPost: function() {
         this.$().hide().show('slow');
-
+        console.log('didInsertElement called');
     }.on('didInsertElement'),
 
-    didRemovePost: function() {
-        var that = this;
-        this.$().hide('slow', function() {
-            that.remove();
-        });
+    deletePost: function() {
+        console.log('this function is called even when didInsertElement event is fired.');
     }.on('willDestroyElement')
 });
