9f23d2768c63b377cd7a6d0249497af6b527184b
Ed <ed@edhuang.com>
2014-10-08 16:11:00 +0800

    updated dashboard to update store after post

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -7,15 +7,13 @@ export default Ember.ArrayController.extend({
     maxLength: 140,
     post: '',
 
-//Computed Property for Number of Letters Left
     wordCount: function() {
         return  this.get('maxLength') - this.get('post').length;
     }.property('post', 'maxLength'),
 
     actions: {
         postTweet: function() {
-
-//Check to see if Post is empty before allowing user to post tweet
+            var id = 1;
             var time = new Date();
             if (this.get('post') !== '') {
                 var post = this.store.createRecord('post', {
@@ -26,6 +24,10 @@ export default Ember.ArrayController.extend({
 
                 post.save();
                 this.set('post', '');
+                
+                var updatedPosts = this.store.all('post');
+                this.set('model', updatedPosts);
+                
             }
         }
     }
